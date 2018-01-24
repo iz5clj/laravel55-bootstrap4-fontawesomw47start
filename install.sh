e3e090cb53a46d29cec3251e6e0a8ee4fd54b241
Michel <ms@auxe.net>
2018-01-24 15:57:34 +0100

    install.sh created

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+echo "Hi, We are going to install the Laravel framework and all his dependencies."
+composer install
+php -r "file_exists('.env') || copy('.env.example', '.env');"
+php artisan key:generate
+
+echo "Running npm will install all the necessary files to have laravel mix runing and also all the necessary files to have bootstrap, jquery, popper and fontawesome."
+npm install
+
+echo "Now edit your .env file to fill in the details about database and all other necessary settings"
